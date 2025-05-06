a93925721681900b8da0316fdd6abb861c5eb839
Akhilesh-Mykalwar <Akhileshmykalwar633@gmail.com>
2025-05-06 23:28:56 +0530

    Add files via upload

---
+++ Contra/tile_button.py
@@ -0,0 +1,7 @@
+from levels import SCREEN_WIDTH, button_col, button_row, i, img_list
+
+
+import button
+
+
+tile_button = button.Button(SCREEN_WIDTH + (75 * button_col) + 50, 75 * button_row + 50, img_list[i], 1)
